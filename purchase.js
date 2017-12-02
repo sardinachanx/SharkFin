3a20dc04ea5b9fdba084047b17f5436772ae1034
Serena Chan <serena.cx13@gmail.com>
2017-12-02 18:21:55 -0500

    Added data dump

--- purchase.js
+++ purchase.js
@@ -40,7 +40,7 @@ exports.purchase = function(item, price, date, category){
   }
 }
 
-exports.binPurchases = function (allPurchases){
+exports.binnedPurchases = function(allPurchases){
   function init(length){
     init_array = [];
     for(i = 0; i < length; i++){
@@ -59,32 +59,62 @@ exports.binPurchases = function (allPurchases){
   return init_array;
 }
 
-exports.isRecurrent = function(queue, label){
-  var sum = 0.0;
-  let currTime = Math.floor((new Date).getTime()/1000);
-  let model = function(epoch_time, start_time){
-    let days_elapsed = Math.floor((epoch_time - start_time)/(60*60*24));
-    if(debug){
-      console.log(label);
-      console.log("start_time " + new Date(start_time * 1000) + " epoch_time " + new Date(epoch_time * 1000));
-      console.log(days_elapsed);
-    }
-    if(days_elapsed <= 30){
-      return 1;
-    }
-    return Math.pow(Math.E, -((days_elapsed - 30)/12));
+exports.getAll = function(queues){
+  //console.log(queues);
+  var all = [];
+  for(i = 0; i < queues.length; i++){
+    all = all.concat(queues[i]);
   }
-  for(let purchase of queue){
-    sum += model(currTime, purchase.date);
+  var sort_by = function(field, reverse, primer){
+    var key = primer ? 
+      function(x) {return primer(x[field])} : 
+      function(x) {return x[field]};
+
+    reverse = !reverse ? 1 : -1;
+
+    return function (a, b) {
+      return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    } 
   }
-  console.log(sum/queue.length);
-  return sum/queue.length > threshold_map[label];
+  all.sort(sort_by('date', true, parseInt));
+  return JSON.stringify(all);
+}
+
+exports.getRecurrent = function(queues, indices){
+  let isRecurrent = function(queue, label){
+    var sum = 0.0;
+    let currTime = Math.floor((new Date).getTime()/1000);
+    let model = function(epoch_time, start_time){
+      let days_elapsed = Math.floor((epoch_time - start_time)/(60*60*24));
+      if(debug){
+        //console.log(label);
+        //console.log("start_time " + new Date(start_time * 1000) + " epoch_time " + new Date(epoch_time * 1000));
+        //console.log(days_elapsed);
+      }
+      if(days_elapsed <= 30){
+        return 1;
+      }
+      return Math.pow(Math.E, -((days_elapsed - 30)/12));
+    }
+    for(let purchase of queue){
+      sum += model(currTime, purchase.date);
+    }
+    console.log("ratio " + sum/queue.length);
+    return sum/queue.length > threshold_map[label];
+  }
+  var recurring = [];
+  for(let i of indices){
+    if(isRecurrent(queues[i],Object.keys(queue_map)[i])){
+      recurring = recurring.concat(queues[i]);
+    }
+  }
+  return JSON.stringify(recurring);
 }
 
 if(debug){
   let p = exports.purchase('kfc', 30, '1506884160','13005032');
   //tag(p);
-  console.log(p.item);
+  //console.log(p.item);
 
   let test_epoch_dates = [/*1512154560,*//*1511636160,*./*1510858560,*//*1510772160,*/1509562560,1509216960,1508612160,1505501760,1503255360];
 
@@ -95,7 +125,11 @@ if(debug){
     }
     return init;
   }
-  //console.log(exports.binPurchases(set()));
+  //console.log(exports.binnedPurchases(set()));
+  let all = exports.binnedPurchases(set());
+  console.log(all);
   console.log(Object.keys(queue_map));
-  console.log(exports.isRecurrent(exports.binPurchases(set())[2],Object.keys(queue_map)[2]))
+  //console.log(exports.isRecurrent(all[2],Object.keys(queue_map)[2]))
+  console.log(exports.getAll(all));
+  console.log(exports.getRecurrent(all,[0,1,2,4]));
 }
