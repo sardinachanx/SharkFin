aa97d349d704f762313a17658f2e8bf640d4a8cb
Rjected <Rjected@users.noreply.github.com>
2017-12-02 03:41:53 -0500

    Added index.js, plaid, and other transaction stuff

---
+++ index.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var data1 = require('./lib/customTransaction1.json');
+
+var purchase = require(__dirname  + '/purchase.js');
+
+var envvar = require('envvar');
+var express = require('express');
+var bodyParser = require('body-parser');
+var moment = require('moment');
+var plaid = require('plaid');
+
+var transactions;
+
+var APP_PORT = envvar.number('APP_PORT', 8000);
+var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID',"5a21a2ac4e95b836d37e3672");
+var PLAID_SECRET = envvar.string('PLAID_SECRET',"c8dbac9121245f93510d139d270519");
+var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY',"b8ef71cf0f8b7bdfe6d8da3761a04a");
+var PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox');
+
+// We store the access_token in memory - in production, store it in a secure
+// persistent data store
+var ACCESS_TOKEN = null;
+var PUBLIC_TOKEN = null;
+var ITEM_ID = null;
+
+var debug = true;
+
+// Initialize the Plaid client
+var client = new plaid.Client(
+  PLAID_CLIENT_ID,
+  PLAID_SECRET,
+  PLAID_PUBLIC_KEY,
+  plaid.environments[PLAID_ENV]
+);
+
+var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', function(request, response, next) {
+  response.render('index.ejs', {
+    PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
+    PLAID_ENV: PLAID_ENV,
+  });
+});
+
+app.post('/get_access_token', function(request, response, next) {
+  console.log(request.body);
+  PUBLIC_TOKEN = request.body.public_token;
+  console.log(PUBLIC_TOKEN);
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error != null) {
+      var msg = 'Could not exchange public_token!';
+      console.log(msg + '\n' + JSON.stringify(error));
+      return response.json({
+        error: msg
+      });
+    }
+    ACCESS_TOKEN = tokenResponse.access_token;
+    ITEM_ID = tokenResponse.item_id;
+    console.log('Access Token: ' + ACCESS_TOKEN);
+    console.log('Item ID: ' + ITEM_ID);
+    response.json({
+      'error': false
+    });
+  });
+});
+
+app.get('/accounts', function(request, response, next) {
+  // Retrieve high-level account information and account and routing numbers
+  // for each account associated with the Item.
+  client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
+    if (error != null) {
+      var msg = 'Unable to pull accounts from the Plaid API.';
+      console.log(msg + '\n' + error);
+      return response.json({
+        error: msg
+      });
+    }
+    console.log(authResponse.accounts);
+    response.json({
+      error: false,
+      accounts: authResponse.accounts,
+      numbers: authResponse.numbers,
+    });
+  });
+});
+
+app.post('/item', function(request, response, next) {
+  // Pull the Item - this includes information about available products,
+  // billed products, webhook information, and more.
+  client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
+
+    // Also pull information about the institution
+    client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
+      if (err != null) {
+        var msg = 'Unable to pull institution information from the Plaid API.';
+        console.log(msg + '\n' + error);
+        return response.json({
+          error: msg
+        });
+      } else {
+        response.json({
+          item: itemResponse.item,
+          institution: instRes.institution,
+        });
+      }
+    });
+  });
+});
+
+app.post('/transactions', function(request, response, next) {
+  // Pull transactions for the Item for the last 30 days
+  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+  var endDate = moment().format('YYYY-MM-DD');
+  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    count: 250,
+    offset: 0,
+  }, function(error, transactionsResponse) {
+    if (error != null) {
+      console.log(JSON.stringify(error));
+      return response.json({
+        error: error
+      });
+    }
+
+    if(debug) transactions = transactionsResponse.transactions.concat(data1.transactions);
+    //console.log('pulled ' + transactionsResponse.transactions.length + ' transactions');
+    
+    transactions = transactions.map(entry => purchase(entry.name, entry.amount, entry.date, entry.category));
+
+    response.json(transactions);
+  });
+});
+
+//POST this endpoint to use custom transaction data
+//app.post('/customTransactions', (_, response) => response.json(data1));
+
+var server = app.listen(APP_PORT, function() {
+  console.log('plaid-walkthrough server listening on port ' + APP_PORT);
+});
