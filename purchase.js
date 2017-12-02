73d4030f0d99ea78be2e44d1c2f8d75b45888fa7
Serena Chan <serena.cx13@gmail.com>
2017-12-02 07:29:56 -0500

    Completed tagging

--- purchase.js
+++ purchase.js
@@ -1,34 +1,41 @@
 const keywords = require("./keywords.json");
 const words = keywords.words;
 const wordmap = keywords.map;
+const category_map = keywords.category_map;
 
 //first tag indiv data, then tag again for long-term trends
-function purchase(item, price, date, categories){
-    let attributes = function(item, price, date, categories){
+function purchase(item, price, date, category){
+    let attributes = function(item, price, date, category){
         let init = [];
         for (let type of Object.keys(words)) {
             for (let key of words[type]) {
                 if (~item.indexOf(key)) {
-                    console.log(item, key, type)
                     init.push(wordmap[type]);
                     break;
                 }
             }
         }
-
+        for(let id of Object.keys(category_map)){
+            if(category.startsWith(id) && !init.includes(category_map[id])){
+                init.push(category_map[id]);
+                break;
+            }
+        }
         return init;
     };
     return{
         item: item,
         price: price,
         date: date,
-        categories: categories,
-        attributes: attributes(item, price, date, categories)
+        category: category,
+        attributes: attributes(item, price, date, category)
     }
 }
 
-/*let p = purchase('wine', 3000, '11th nov',[]);
+
+
+let p = purchase('apples', 3, '11th nov','19047000');
 //tag(p);
-console.log(p);*/
+console.log(p);
 
 module.exports = purchase;
