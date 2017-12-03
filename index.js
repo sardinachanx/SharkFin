3cce090fffd2e475d45f9ce3fa09dfbcdbc71b44
Rjected <Rjected@users.noreply.github.com>
2017-12-03 09:52:09 -0500

    fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var data1 = require('./lib/plaidSandbox1.json');
+var data1 = require('./lib/plaidSandbox2.json');
 
 var purchase = require('./purchase.js');
 var fetchAlts = require('./fetchAlts.js');
